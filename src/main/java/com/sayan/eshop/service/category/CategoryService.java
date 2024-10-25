270cebe36c99c093f78e30ae697823373684ce4d
Sayan Mondal <sayan.mondal4557@gmail.com>
2024-10-25 20:55:39 +0530

    created category service interface and it's implementation class

--- src/main/java/com/sayan/eshop/service/category/CategoryService.java
+++ src/main/java/com/sayan/eshop/service/category/CategoryService.java
@@ -1,4 +1,14 @@
 package com.sayan.eshop.service.category;
 
+import com.sayan.eshop.model.Category;
+
+import java.util.List;
+
 public interface CategoryService {
+    Category getCategoryById(Integer id);
+    Category getCategoryByName(String name);
+    List<Category> getAllCategories();
+    Category saveCategory(Category category);
+    Category updateCategory(Category category);
+    void deleteCategoryById(Integer id);
 }
--- src/main/java/com/sayan/eshop/service/category/CategoryServiceImpl.java
+++ src/main/java/com/sayan/eshop/service/category/CategoryServiceImpl.java
@@ -1,4 +1,50 @@
 package com.sayan.eshop.service.category;
 
-public class CategoryServiceImpl {
+import com.sayan.eshop.exception.ResourceNotFoundException;
+import com.sayan.eshop.model.Category;
+import com.sayan.eshop.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+    private final CategoryRepository categoryRepository;
+    @Override
+    public Category getCategoryById(Integer id) {
+        return categoryRepository
+                .findById(id)
+                .orElseThrow(()->new ResourceNotFoundException("Category Not Found"));
+    }
+
+    @Override
+    public Category getCategoryByName(String name) {
+        return categoryRepository.findByName(name);
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public Category saveCategory(Category category) {
+        return null;
+    }
+
+    @Override
+    public Category updateCategory(Category category) {
+        return null;
+    }
+
+    @Override
+    public void deleteCategoryById(Integer id) {
+        categoryRepository
+                .findById(id)
+                .ifPresentOrElse(categoryRepository::delete, ()->{
+                    throw new ResourceNotFoundException("Category Not Found");
+                });
+    }
 }
